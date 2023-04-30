d29ac90380e52d7724222a5912e40698522d7ed4
Devender Singh <99714664+DevendersinghRajput@users.noreply.github.com>
2023-04-30 11:40:30 +0530

    test

--- packers-and-movers-in-hisar.php
+++ packers-and-movers-in-hisar.php
@@ -57,8 +57,8 @@
     <?php include'include/stactic-meta.php'; ?>
     
     <!-- ====== css link ======= -->
-    <link rel="stylesheet" href="css/city.css?version=0.2.3">
-    <link rel="preload" href="css/city.css?version=0.2.3" as="style"> 
+    <link rel="stylesheet" href="css/city.css?version=0.2.4">
+    <link rel="preload" href="css/city.css?version=0.2.4" as="style"> 
     <!-- =========== change meta tages ======== -->
     <title>Best Packers and movers in Hisar - ShiftingWay Packers</title>
     <meta name="description" content="Whether moving locally or long-distance in Hisar, Shiftingway provides customized solutions. Contact us today for a seamless and hassle-free home shifting." />
