e5cc95d33db748814bad501ece1752517b6bc12b
Devender Singh <99714664+DevendersinghRajput@users.noreply.github.com>
2023-06-06 06:05:09 +0530

    mobile no. add vikash adn remov my no.

--- contact.php
+++ contact.php
@@ -186,7 +186,7 @@
                             </div>
                         </a>
                         
-                        <a href="tel:+91 9817326797" class="contact-mail-box r-flex ali-c jut-sb" onclick="gtag('event', 'contect_us_call');">
+                        <a href="tel:+91 8222061512" class="contact-mail-box r-flex ali-c jut-sb" onclick="gtag('event', 'contect_us_call');">
                             <div class="contact-mail-item c-flex ">
                                 <span class="contact-mail-head">Call us</span>
                                 <span class="contact-mail-cont">+91 98173 26797</span>
